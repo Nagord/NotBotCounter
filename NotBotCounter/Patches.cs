501aed2d6fca423c3b1f708954b37b23c5b7575c
DragonFire47 <1redyoung@gmail.com>
2020-11-04 13:46:21 -0500

    Add project files.

---
+++ NotBotCounter/Patches.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using HarmonyLib;
+using static PulsarPluginLoader.Patches.HarmonyHelpers;
+
+namespace NotBotCounter
+{
+    [HarmonyPatch(typeof(PLServer), "EditGameSettings")]
+    class EditGameSettingsPatch
+    {
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+
+            List<CodeInstruction> targetSequence = new List<CodeInstruction>()
+            {
+                new CodeInstruction(OpCodes.Ldloc_0),
+                new CodeInstruction(OpCodes.Add),
+            };
+
+            List<CodeInstruction> injectedSequence = new List<CodeInstruction>()
+            {
+
+            };
+            return PatchBySequence(instructions, targetSequence, injectedSequence, PatchMode.REPLACE);
+        }
+    }
+    [HarmonyPatch(typeof(PLServer), "Update")]
+    class UpdatePatch
+    {
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+
+            List<CodeInstruction> targetSequence = new List<CodeInstruction>()
+            {
+                new CodeInstruction(OpCodes.Callvirt, AccessTools.Method(typeof(Room), "get_PlayerCount")),
+                new CodeInstruction(OpCodes.Ldloc_S),
+                new CodeInstruction(OpCodes.Add),
+            };
+
+            List<CodeInstruction> injectedSequence = new List<CodeInstruction>()
+            {
+                new CodeInstruction(OpCodes.Callvirt, AccessTools.Method(typeof(Room), "get_PlayerCount")),
+            };
+            return PatchBySequence(instructions, targetSequence, injectedSequence, PatchMode.REPLACE, CheckMode.NONNULL);
+        }
+    }
+}
---
+++ NotBotCounter/Plugin.cs
@@ -0,0 +1,20 @@
+﻿using PulsarPluginLoader;
+
+namespace NotBotCounter
+{
+    public class Plugin : PulsarPlugin
+    {
+        public override string Version => "1.0.0";
+
+        public override string Author => "Dragon";
+
+        public override string LongDescription => "Removes bots from the lobby player counter (allows players to join regardles of bots filling game)";
+
+        public override string Name => "NotBotCounter";
+
+        public override string HarmonyIdentifier()
+        {
+            return "Dragon.NotBotCounter";
+        }
+    }
+}
