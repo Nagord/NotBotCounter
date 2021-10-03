8be217c142ed0747c39c64f1f97d5e653678399c
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2021-10-03 19:59:13 -0400

    -Updated for PML 0.10.0 -Incrimented Version

--- NotBotCounter/Mod.cs
+++ NotBotCounter/Mod.cs
@@ -1,10 +1,10 @@
-﻿using PulsarPluginLoader;
+﻿using PulsarModLoader;
 
 namespace NotBotCounter
 {
-    public class Plugin : PulsarPlugin
+    public class Mod : PulsarMod
     {
-        public override string Version => "1.0.0";
+        public override string Version => "1.0.1";
 
         public override string Author => "Dragon";
 
